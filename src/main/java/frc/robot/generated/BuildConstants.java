725c9b352b8cd169abe5b59a7823173e8c56dc76
chary <smkzachatac@gmail.com>
2024-11-06 20:30:05 -0800

    Added BuildConstants

---
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.generated;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025-Beta";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "0d132e116688f840a88f81fabaf9794fa9506527";
+  public static final String GIT_DATE = "2024-11-05 22:19:54 PST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-11-05 22:21:22 PST";
+  public static final long BUILD_UNIX_TIME = 1730874082782L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
