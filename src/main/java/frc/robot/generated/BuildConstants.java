d4f827258e7c90ef6eab4d2dc4d90ea0d5eeb565
charySHS <smkzachatac@gmail.com>
2024-11-09 10:46:35 -0800

    Old BuildConstants

---
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.generated;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "IJ";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "cbeb50a32876db666b157f2a9ed900023406f8c4";
+  public static final String GIT_DATE = "2024-11-05 19:35:17 PST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-11-05 20:10:28 PST";
+  public static final long BUILD_UNIX_TIME = 1730866228955L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
